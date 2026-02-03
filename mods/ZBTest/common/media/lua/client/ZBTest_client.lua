d061f4e967f923661a8b982c95dba6cb388ab008
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-02-03 18:50:03 +0200

    upd

--- mods/ZBTest/common/media/lua/client/ZBTest_client.lua
+++ mods/ZBTest/common/media/lua/client/ZBTest_client.lua
@@ -2,11 +2,13 @@ if debugScenarios == nil then
     debugScenarios = {}
 end
 
+UIManager.setShowLuaDebuggerOnError(false)
+
 debugScenarios.DebugScenarioTGreen = {
     forceLaunch = true, -- XXX
     name = "Turbo Green Test",
     world = "Muldraugh, KY",
-    startLoc = {x=8496, y=5789, z=0 }, -- other 11989x6903
+    startLoc = {x=8496, y=5789, z=0},
     setSandbox = function()
         SandboxVars.VehicleEasyUse = true;
         SandboxVars.Zombies = 6;
