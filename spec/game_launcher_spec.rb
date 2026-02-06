3e20e50fad3a857af36cd03219e142a1351b5d01
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-02-06 21:34:14 +0200

    fix specs

--- spec/game_launcher_spec.rb
+++ spec/game_launcher_spec.rb
@@ -16,10 +16,12 @@ RSpec.describe ZBSpec::GameLauncher do
   end
   
   let(:launcher) { described_class.new(config) }
-  let(:pid_file) { File.join('tmp', 'zbspec.pid') }
+  let(:cache_dir) { File.expand_path('./tmp/test_cache') }
+  let(:pid_file) { File.join(cache_dir, 'pz.pid') }
 
   before do
-    # Clean up any existing PID file
+    # Ensure cache dir exists and clean up any existing PID file
+    FileUtils.mkdir_p(cache_dir)
     File.delete(pid_file) if File.exist?(pid_file)
   end
 
@@ -29,6 +31,11 @@ RSpec.describe ZBSpec::GameLauncher do
   end
 
   describe '#start with PID file management' do
+    before do
+      # Mock init_cachedir to avoid file system side effects
+      allow(launcher).to receive(:init_cachedir)
+    end
+
     context 'when no PID file exists' do
       it 'starts a new process and creates PID file' do
         allow(launcher).to receive(:find_executable).and_return('/fake/game')
@@ -45,7 +52,7 @@ RSpec.describe ZBSpec::GameLauncher do
     context 'when PID file exists with running process' do
       it 'reuses the existing process' do
         # Write a PID file with the current process (which is alive)
-        FileUtils.mkdir_p('tmp')
+        FileUtils.mkdir_p(cache_dir)
         File.write(pid_file, Process.pid.to_s)
         
         expect(launcher).not_to receive(:spawn)
@@ -59,7 +66,7 @@ RSpec.describe ZBSpec::GameLauncher do
     context 'when PID file exists with dead process' do
       it 'removes stale PID file and starts new process' do
         # Write a PID file with a process that doesn't exist
-        FileUtils.mkdir_p('tmp')
+        FileUtils.mkdir_p(cache_dir)
         File.write(pid_file, '999999')
         
         allow(launcher).to receive(:find_executable).and_return('/fake/game')
@@ -76,13 +83,15 @@ RSpec.describe ZBSpec::GameLauncher do
   describe '#stop' do
     it 'removes PID file when stopping' do
       # Create a PID file
-      FileUtils.mkdir_p('tmp')
+      FileUtils.mkdir_p(cache_dir)
       File.write(pid_file, '12345')
       launcher.instance_variable_set(:@pid, 12345)
       launcher.instance_variable_set(:@running, true)
       
-      allow(Process).to receive(:kill)
-      allow(Process).to receive(:wait)
+      # Mock Process.kill for both process_alive? check (signal 0) and actual kill
+      allow(Process).to receive(:kill).with(0, 12345).and_return(1)
+      allow(Process).to receive(:kill).with('TERM', 12345)
+      allow(Process).to receive(:wait).with(12345, Process::WNOHANG).and_return(12345)
       
       launcher.stop
       
@@ -96,41 +105,42 @@ RSpec.describe ZBSpec::GameLauncher do
       expect(mod_list.first).to eq('ZombieBuddy')
     end
 
-    it 'includes ZBSpec after ZombieBuddy' do
+    it 'includes ZBetterFPS and ZBSpec after ZombieBuddy' do
       mod_list = launcher.send(:build_mod_list)
       expect(mod_list[0]).to eq('ZombieBuddy')
-      expect(mod_list[1]).to eq('ZBSpec')
+      expect(mod_list[1]).to eq('ZBetterFPS')
+      expect(mod_list[2]).to eq('ZBSpec')
     end
 
-    it 'adds user mods after ZombieBuddy and ZBSpec' do
+    it 'adds user mods after default mods' do
       mod_list = launcher.send(:build_mod_list)
-      expect(mod_list).to eq(['ZombieBuddy', 'ZBSpec', 'TestMod', 'AnotherMod'])
+      expect(mod_list).to eq(['ZombieBuddy', 'ZBetterFPS', 'ZBSpec', 'TestMod', 'AnotherMod'])
     end
 
     it 'does not duplicate ZombieBuddy if user includes it' do
       config['mods'] = ['ZombieBuddy', 'TestMod']
       mod_list = launcher.send(:build_mod_list)
       expect(mod_list.count('ZombieBuddy')).to eq(1)
-      expect(mod_list).to eq(['ZombieBuddy', 'ZBSpec', 'TestMod'])
+      expect(mod_list).to eq(['ZombieBuddy', 'ZBetterFPS', 'ZBSpec', 'TestMod'])
     end
 
     it 'does not duplicate ZBSpec if user includes it' do
       config['mods'] = ['ZBSpec', 'TestMod']
       mod_list = launcher.send(:build_mod_list)
       expect(mod_list.count('ZBSpec')).to eq(1)
-      expect(mod_list).to eq(['ZombieBuddy', 'ZBSpec', 'TestMod'])
+      expect(mod_list).to eq(['ZombieBuddy', 'ZBetterFPS', 'ZBSpec', 'TestMod'])
     end
 
     it 'works with no user mods' do
       config['mods'] = []
       mod_list = launcher.send(:build_mod_list)
-      expect(mod_list).to eq(['ZombieBuddy', 'ZBSpec'])
+      expect(mod_list).to eq(['ZombieBuddy', 'ZBetterFPS', 'ZBSpec'])
     end
 
     it 'works with nil mods' do
       config['mods'] = nil
       mod_list = launcher.send(:build_mod_list)
-      expect(mod_list).to eq(['ZombieBuddy', 'ZBSpec'])
+      expect(mod_list).to eq(['ZombieBuddy', 'ZBetterFPS', 'ZBSpec'])
     end
   end
 end
