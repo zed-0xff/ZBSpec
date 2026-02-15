b2fb7090d3bfbbe0ad0a16b1367a373b3cf22dea
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-02-16 01:51:32 +0200

    better autojoin

--- mods/ZBSpec/common/media/lua/client/ZBSpec_client_MP.lua
+++ mods/ZBSpec/common/media/lua/client/ZBSpec_client_MP.lua
@@ -1,73 +1,84 @@
 -- autoconnect to server
+require "zbsHook"
+
+local firstTimeTbl = {
+}
+
+-- server connect
 require "OptionScreens/ServerConnectPopup"
 
-local conn_firstTime = true
-local orig_setVisible = ServerConnectPopup.setVisible
-print("[d] orig_setVisible =", orig_setVisible)
-
-function ServerConnectPopup:setVisible(visible)
-    print("[d] ServerConnectPopup:setVisible(", visible, ")")
-    if conn_firstTime and visible then
-        conn_firstTime = false
-        orig_setVisible(self, true)
-        self.usernameEntry:setText("admin")
-        self.passwordEntry:setText("zbspec")
-        self:onOptionMouseDown(self.connectBtn)
-        return
+zbsHook(ServerConnectPopup, {
+    setVisible = function(orig, self, visible, ...)
+        print("[d] ServerConnectPopup:setVisible(", visible, ")")
+        if visible and not firstTimeTbl[self] then
+            firstTimeTbl[self] = true
+            orig(self, visible, ...)
+            self.usernameEntry:setText("admin")
+            self.passwordEntry:setText("zbspec")
+            self:onOptionMouseDown(self.connectBtn)
+            return
+        end
+        orig(self, visible, ...)
     end
-    orig_setVisible(self, visible)
-end
+})
+
+-- select world
+require "OptionScreens/WorldSelect"
+zbsHook(WorldSelect, {
+    hasChoices = function()
+        return false -- skip world selection
+    end
+})
 
 -- select map
 require "OptionScreens/MapSpawnSelect"
 
-local spawn_firstTime = true
-local orig_MapSpawnSelect_setVisible = MapSpawnSelect.setVisible
-print("[d] orig_MapSpawnSelect_setVisible =", orig_MapSpawnSelect_setVisible)
+zbsHook(MapSpawnSelect, {
+    hasChoices = function()
+        return false -- skip map selection
+    end,
 
-function MapSpawnSelect:setVisible(visible)
-    print("[d] MapSpawnSelect:setVisible(", visible, ")")
-    if spawn_firstTime and visible then
-        spawn_firstTime = false
-        orig_MapSpawnSelect_setVisible(self, true)
-        self:onOptionMouseDown(self.nextButton)
-        return
+    setVisible = function(orig, self, visible, ...)
+        print("[d] MapSpawnSelect:setVisible(", visible, ")")
+        if visible and not firstTimeTbl[self] then
+            firstTimeTbl[self] = true
+            orig(self, visible, ...)
+            self:onOptionMouseDown(self.nextButton)
+            return
+        end
+        orig(self, visible, ...)
     end
-    orig_MapSpawnSelect_setVisible(self, visible)
-end
+})
+
 
 -- perks/skills
 require "OptionScreens/CharacterCreationProfession"
 
-local prof_firstTime = true
-local orig_CharacterCreationProfession_setVisible = CharacterCreationProfession.setVisible
-print("[d] orig_CharacterCreationProfession_setVisible =", orig_CharacterCreationProfession_setVisible)
-
-function CharacterCreationProfession:setVisible(visible)
-    print("[d] CharacterCreationProfession:setVisible(", visible, ")")
-    if prof_firstTime and visible then
-        prof_firstTime = false
-        orig_CharacterCreationProfession_setVisible(self, true)
-        self:onOptionMouseDown(self.playButton)
-        return
+zbsHook(CharacterCreationProfession, {
+    setVisible = function(orig, self, visible, ...)
+        print("[d] CharacterCreationProfession:setVisible(", visible, ")")
+        if visible and not firstTimeTbl[self] then
+            firstTimeTbl[self] = true
+            orig(self, visible, ...)
+            self:onOptionMouseDown(self.playButton)
+            return
+        end
+        orig(self, visible, ...)
     end
-    orig_CharacterCreationProfession_setVisible(self, visible)
-end
+})
 
 -- appearance/sex
 require "OptionScreens/CharacterCreationMain"
 
-local perks_firstTime = true
-local orig_CharacterCreationMain_setVisible = CharacterCreationMain.setVisible
-print("[d] orig_CharacterCreationMain_setVisible =", orig_CharacterCreationMain_setVisible)
-
-function CharacterCreationMain:setVisible(visible)
-    print("[d] CharacterCreationMain:setVisible(", visible, ")")
-    if perks_firstTime and visible then
-        perks_firstTime = false
-        orig_CharacterCreationMain_setVisible(self, true)
-        self:onOptionMouseDown(self.playButton)
-        return
+zbsHook(CharacterCreationMain, {
+    setVisible = function(orig, self, visible, ...)
+        print("[d] CharacterCreationMain:setVisible(", visible, ")")
+        if visible and not firstTimeTbl[self] then
+            firstTimeTbl[self] = true
+            orig(self, visible, ...)
+            self:onOptionMouseDown(self.playButton)
+            return
+        end
+        orig(self, visible, ...)
     end
-    orig_CharacterCreationMain_setVisible(self, visible)
-end
+})
